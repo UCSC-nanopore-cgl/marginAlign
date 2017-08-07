48a798a092b131ee3d421a69b382b32d61b3a6aa
Andrew Bailey <bailey.andrew4@gmail.com>
2017-08-07 10:12:20 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name="marginAlign",
-      version="1.2.0",
+      version="1.2.1",
       description="Toil-based functions for performing MinION sequence analysis",
       author="Benedict Paten and Art Rand",
       author_email="arand@soe.ucsc.edu",
       url="https://github.com/UCSC-nanopore-cgl/marginAlign",
-      packages=['src'],
+      package_dir={"": "src"},
+      packages=find_packages("src")
       # install_requires=["numpy>=1.9.2",
       #                   "h5py>=2.2.1",
       #                   "pysam>=0.8.2.1",
